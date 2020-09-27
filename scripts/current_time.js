b7b90d7eae8c31cfa837ebc6d253993fbe3b4b7c
greenhunt2003 <medinaemmanruiz@gmail.com>
2020-09-27 10:25:17 +0000

    modified current_time.js

--- scripts/current_time.js
+++ scripts/current_time.js
@@ -1,4 +1,6 @@
 var time = new Date();
-
 var currentyear = time.getFullYear();
+var currentdate = time;
+
 document.getElementById("current_year").innerHTML = currentyear;
+document.getElementById("current_date").innerHTML = currentdate;
